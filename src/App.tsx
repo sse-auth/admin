4e9c0e4c7025fcd1c2b55ba9942dc96d33962fe4
sseworld <suryakantmaurya2007@gmail.com>
2024-06-25 19:41:50 +0530

    Auth Portion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import React from "react"
+import { Route, Routes, useLocation } from "react-router-dom"
+import { Login, Register, Settings, Profile } from "./pages/user"
+import Main from "./pages/dashboard"
+import Loader from "./common/Loader"
+
 function App() {
-  return <h1>Hello WOrld</h1>
+  const [loading, setLoading] = React.useState<boolean>(true)
+  const { pathname } = useLocation()
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  React.useEffect(() => {
+    setTimeout(() => setLoading(false), 1000)
+  }, [])
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <Routes>
+      <Route index element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/setting" element={<Settings />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/pages/user/index.ts
@@ -0,0 +1,6 @@
+import Login from "./Login"
+import Register from "./Register"
+import Profile from "./Profile"
+import Settings from "./Settings"
+
+export { Login, Register, Profile, Settings }
